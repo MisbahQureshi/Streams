f1afbd0d01667dfdee24f43b098ee82bed7e29c9
Misbah Qureshi <106266858+MisbahQureshi@users.noreply.github.com>
2026-03-15 22:25:20 -0400

    Add distinct and set collection methods for list

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -7,6 +7,8 @@ class Main {
     public static void main(String[] args) {
        
         List<Integer> nums = Arrays.asList(9,1,4,5,6,5,7,2,3,4,5,9,7,3);
+
+        System.out.println(nums.stream().distinct().toList());
         
         System.out.println(nums.stream().collect(Collectors.toCollection(() -> new LinkedHashSet<Integer>())));
         
